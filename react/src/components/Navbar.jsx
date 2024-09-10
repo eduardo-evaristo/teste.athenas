aa148a095a342b8346e4edd82679173103147f93
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2024-09-10 03:48:49 -0300

    fix: fix navbar not showing up on small screens

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -38,10 +38,11 @@ export default function Navbar() {
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
+            aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbar">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               {isUserLoggedIn ? (
                 <>
